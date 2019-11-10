1ea157627e76bbf63a79d7a45432a48d6472dd07
jLopezbarb <j.lopezbarb@gmail.com>
2019-11-11 00:09:24 +0100

    Fix getPieces from color now takes color in count

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Board.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Board.java
@@ -48,10 +48,12 @@ class Board {
     }
 
     List<Piece> getPieces(Color color) {
-        List<Piece> pieces = new ArrayList<Piece>();
+        List<Piece> pieces = new ArrayList<>();
         for (int i = 0; i < this.getDimension(); i++) {
             for (int j = 0; j < this.getDimension(); j++) {
-                pieces.add(this.squares[i][j].getPiece());
+                if (this.squares[i][j].getPiece().getColor() == color){
+                    pieces.add(this.squares[i][j].getPiece());
+                }
             }
         }
 		return pieces;

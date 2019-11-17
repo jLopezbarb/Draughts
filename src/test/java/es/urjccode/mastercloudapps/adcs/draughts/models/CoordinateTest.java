925c69c53882b2958dbc8661ece29399db2c7a36
jLopezbarb <j.lopezbarb@gmail.com>
2019-11-18 00:14:31 +0100

    Refactored test to pass

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/CoordinateTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/CoordinateTest.java
@@ -1,6 +1,7 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,10 +9,11 @@ public class CoordinateTest {
 
     @Test
     public void testGivenTwoCoordinatesWhenBettweenDiagonalThenOk() {
-        assertEquals(new Coordinate(1, 1), new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 0)));
-        assertEquals(new Coordinate(3, 1), new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 0)));
-        assertEquals(new Coordinate(3, 3), new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 4)));
-        assertEquals(new Coordinate(1, 3), new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 4)));
+        assertTrue(new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 0)).contains(new Coordinate(1, 1)));
+        assertTrue(new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 0)).contains(new Coordinate(3, 1)));
+        assertTrue(new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 4)).contains(new Coordinate(3, 3)));
+        assertTrue(new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 4)).contains(new Coordinate(1, 3)));
+
     }
 
     @Test
